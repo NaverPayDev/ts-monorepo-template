5938eb12708f208766808eba1c9eb64683325151
oneweek-lee <oneweek.lee@navercorp.com>
2024-03-05 15:03:38 +0900

    코드 추가

--- packages/random/src/index.ts
+++ packages/random/src/index.ts
@@ -1,2 +1,8 @@
 // get 0~9 integer
-export const getDigit = () => Math.floor(Math.random() * 10)
+export const getDigit = () => Math.floor(Math.random() * 10);
+
+export const generateDigit = () => Math.floor(Math.random() * 10);
+
+export function get10Digit() {
+    return Array.from({ length: 10 }).map(() => getDigit());
+}
